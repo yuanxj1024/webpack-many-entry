0a7e60cc541f4dd202f453a568c3975ab121c1aa
yuanxj1024 <xuanyuanziruo@gmail.com>
2016-07-26 10:28:11 +0800

    fix

--- bin/webpack.dev.config.js
+++ bin/webpack.dev.config.js
@@ -29,72 +29,73 @@ plugins.push(extractSASS);
 
 // 处理jade页面
 for (var chunkname in pages) {
-  var conf = {
-    filename: chunkname + '.html',
-    template: pages[chunkname],
-    inject: true,
-    minify: {
-      removeComments: true,
-      collapseWhitespace: false
-    },
-    // chunks: ['common', chunkname],
-    hash: true,
-    globalConfig: globalConfig
-  }
-  // plugins.push(new HtmlWebpackPlugin(conf));
+    console.log('chunkname=', chunkname);
+    var conf = {
+        filename: chunkname + '.html',
+        template: pages[chunkname],
+        inject: true,
+        minify: {
+            removeComments: true,
+            collapseWhitespace: false
+        },
+        chunks: [chunkname],
+        hash: true,
+        globalConfig: globalConfig
+    }
+    plugins.push(new HtmlWebpackPlugin(conf));
 }
 
 /**
  * 配置webpack
  */
 var config = {
-  entry: entrys,
-  // entry: {
-  //   // '360/20150601p1/index': './app/src/360/20150601p1/index.js'
-  //   // '360/20150722p2/defaut': './app/src/360/20150722p2/index.js'
-  //   '20150601p1': './app/src/360/20150601p1/index.js',
-  //   '20150722p2': './app/src/360/20150722p2/index.js'
-  // },
-  output: {
-    path: path.resolve(containerPath, './app/www/'),
-    publicPath: './',
-    filename: '[name].js'
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [{
-        test: /\.html$/,
-        loader: 'raw',
-        exclude: /(node_modules)/
-      },
-      // {
-      //   test: /\.js$/,
-      //   loader: 'eslint-loader',
-      //   exclude: /(node_modules)/
-      // },
-      {
-        test: /\.scss$/i,
-        loader: extractSASS.extract(['css', 'sass'])
-      }, {
-        test: /.jade$/,
-        loader: 'jade-loader',
-        exclude: /(node_modules)/
-      }, {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'url-loader?limit=8192&name=images/[name].[ext]'
-      }
-    ]
-  },
-  plugins: plugins,
-  resolve: {
-    // alias: alias,
-    extensions: ['', '.js', '.css', '.scss', '.jade', '.png', '.jpg']
-  },
-  externals: {
-    jquery: 'window.jQuery',
-    // backbone: 'window.Backbone',
-    underscore: 'window._',
-    webim: 'window.webim'
-  }
+    entry: entrys,
+    // entry: {
+    //   // '360/20150601p1/index': './app/src/360/20150601p1/index.js'
+    //   // '360/20150722p2/defaut': './app/src/360/20150722p2/index.js'
+    //   '20150601p1': './app/src/360/20150601p1/index.js',
+    //   '20150722p2': './app/src/360/20150722p2/index.js'
+    // },
+    output: {
+        path: path.resolve(containerPath, './app/www/'),
+        publicPath: './',
+        filename: '[name].js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+                test: /\.html$/,
+                loader: 'raw',
+                exclude: /(node_modules)/
+            },
+            // {
+            //   test: /\.js$/,
+            //   loader: 'eslint-loader',
+            //   exclude: /(node_modules)/
+            // },
+            {
+                test: /\.scss$/i,
+                loader: extractSASS.extract(['css', 'sass'])
+            }, {
+                test: /.jade$/,
+                loader: 'jade-loader',
+                exclude: /(node_modules)/
+            }, {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader?limit=8192&name=images/[name].[ext]'
+            }
+        ]
+    },
+    plugins: plugins,
+    resolve: {
+        // alias: alias,
+        extensions: ['', '.js', '.css', '.scss', '.jade', '.png', '.jpg']
+    },
+    externals: {
+        jquery: 'window.jQuery',
+        // backbone: 'window.Backbone',
+        underscore: 'window._',
+        webim: 'window.webim'
+    }
 };
 module.exports = config;
