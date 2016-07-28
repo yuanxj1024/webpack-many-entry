67d0fd61a15056a2f27045dfc4d1a71e8f1b1b5c
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-07-28 17:33:19 +0800

    修正eslint 检查alias问题

--- app/src/360/20150601p1/index.js
+++ app/src/360/20150601p1/index.js
@@ -1,7 +1,8 @@
+var a = require('./style.scss');
+var b = require('./js/main.js');
+var c = require('tip');
+
 $(function () {
-  var a = require('./style.scss');
-  var b = require('./js/main.js');
-  var c = require('tip');
   console.log('c is ', c);
   console.log('ready');
   console.log(a, b);
