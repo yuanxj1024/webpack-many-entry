928c82900970183600169ea604aef4918c00f3c3
Aaron-vk-Yuan <xuanyuanziruo@gmail.com>
2016-07-28 10:08:33 +0800

    eslint

--- app/src/360/20150601p1/index.js
+++ app/src/360/20150601p1/index.js
@@ -1,10 +1,8 @@
-var a = require('./style.scss');
-var b = require('./js/main.js');
-var c = require('tip');
-
-console.log('c is ', c);
-
-// $(function () {
+$(function () {
+  var a = require('./style.scss');
+  var b = require('./js/main.js');
+  var c = require('tip');
+  console.log('c is ', c);
   console.log('ready');
   console.log(a, b);
-// });
+});
